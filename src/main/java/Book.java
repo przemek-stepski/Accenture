7b061a17dff90fb4bd4ca0d7bec06c4c9ca41093
Przemek Stepski <przemek.stepski@wp.pl>
2021-11-03 22:39:48 +0100

    Initial commit

---
+++ src/main/java/Book.java
@@ -0,0 +1,75 @@
+import java.util.Objects;
+
+    public class Book implements Comparable<Book>{
+        private final String title;
+        private final String author;
+        private final String isbn;
+        private String lastBorrowedDate;
+        private String borrower;
+
+        public Book(String title, String author, String ISBN) {
+            this.title = title;
+            this.author = author;
+            this.isbn = ISBN;
+            lastBorrowedDate = "0000-01-01";
+            borrower = "never borrowed";
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getAuthor() {
+            return author;
+        }
+
+        public String getIsbn() {
+            return isbn;
+        }
+
+        public String getLastBorrowedDate() {
+            return lastBorrowedDate;
+        }
+
+        public String getBorrower() {
+            return borrower;
+        }
+
+        public void setLastBorrowedDate(String lastBorrowedDate) {
+            this.lastBorrowedDate = lastBorrowedDate;
+        }
+
+        public void setBorrower(String borrower) {
+            this.borrower = borrower;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (this.getClass() != o.getClass() || o == null) return false;
+
+            Book book = (Book) o;
+            return Objects.equals(title, book.title) && Objects.equals(author, book.author) && Objects.equals(isbn, book.isbn);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(title, author, isbn);
+        }
+
+        @Override
+        public String toString() {
+            return "Book{" +
+                    "title='" + title + '\'' +
+                    ", author='" + author + '\'' +
+                    ", isbn='" + isbn + '\'' +
+                    ", lastBorrowedDate='" + lastBorrowedDate + '\'' +
+                    ", borrower='" + borrower + '\'' +
+                    '}';
+        }
+
+        @Override
+        public int compareTo(Book book) {
+            return getIsbn().compareTo(book.isbn);
+        }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Book book1 = new Book("1","1","1");
+        Book book2 = new Book("2","2","2");
+
+        System.out.println(book1.compareTo(book1));
+
+    }
+}
